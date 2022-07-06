e974c26effd0074a8bd95c1afa124dd26c36e9d6
gustavo <gus09ayala@gmail.com>
2022-07-06 14:42:22 -0400

    This is a bad commit This is terrible, commit more often, anyway etch a sketch v1 i guess

--- scrips.js
+++ scrips.js
@@ -1,12 +1,35 @@
 const container = document.querySelector(".container");
+const clearBtn = document.getElementById("clear-Btn")
 
 function createDiv(divs){
+    const width = ((960/ Math.sqrt(divs)) / 960)*100;
+    console.log(width)
     for (let i = 1; i <= divs; i++){
         const newDiv = document.createElement("div");
         newDiv.classList.add("grid-Div")
-        newDiv.textContent= i;
+        //newDiv.textContent= i;
+        newDiv.setAttribute("onmouseover", "activatedColor(this)")
+        newDiv.style.width = `${width}%`
         container.appendChild(newDiv)
 
     }
 }
 createDiv(256);
+
+function activatedColor(element){
+    element.style.backgroundColor = "plum";
+}
+
+clearBtn.onclick = () => {
+    let newBoard = prompt("How many squares per side do you want?");
+    if (newBoard === null){
+        return
+    }
+    if (newBoard > 100) {
+        alert("Input out of range (must be no greater than 100)");
+        return
+    }
+    container.innerHTML= "";
+    newBoard = newBoard * newBoard;
+    createDiv(newBoard);
+}
